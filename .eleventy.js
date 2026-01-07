aff0d8696861858730b189bcb55c3128cff63ae2
Heydon <heydon@heydonworks.com>
2026-01-07 17:43:14 +0000

    md filter protection, methods in layout

--- .eleventy.js
+++ .eleventy.js
@@ -39,7 +39,7 @@ module.exports = async function(eleventyConfig) {
   });
 
   eleventyConfig.addFilter('md', content => {
-    return md.render(content);
+    return content ? md.render(content) : '';
   });
 
     eleventyConfig.addFilter('lowercase', content => {

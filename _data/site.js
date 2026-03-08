47d142ac4555d88cbbdccbe115bf40a0d83f76b1
Heydon <heydon@heydonworks.com>
2026-03-08 11:18:07 +0000

    blamsample -> blamsource

--- _data/site.js
+++ _data/site.js
@@ -7,7 +7,7 @@ module.exports = {
     Sampling: 'Elements used for creating and playing sound sources.',
     Modulating: 'Elements that make gradual changes to parameters over time.',
     Processing: 'Elements involved in affecting the character of sounds. Effects, essentially.',
-    Controlling: 'Interface elements for triggering functions, changing props and parameters, handling MIDI, and displaying values.',
+    Controlling: 'Elements for triggering functions, changing props and parameters, and handling MIDI.',
     Automating: 'Elements for changing and randomizing props and parameters automatically (according to events).',
     Visualizing: 'Elements involved in rendering sound and state data in some way.'
   }

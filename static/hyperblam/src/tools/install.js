bf7716cca1538f6ef45ff17cf3905de1109c23bf
Heydon <Heydon@users.noreply.github.com>
2026-05-22 16:37:41 +0000

    deploy: 6324623a0c70d79a0f30c78093bba7668ac6f822

--- static/hyperblam/src/tools/install.js
+++ static/hyperblam/src/tools/install.js
@@ -4,7 +4,9 @@ const install = async url => {
   const blamElements = undefinedElements.filter(elem => elem.nodeName.includes(`-BLAM`));
   const uniqueNames = [...new Set(blamElements.map(elem => elem.nodeName.toLowerCase()))];
   for (const name of uniqueNames) {
-    await import(`${path}/elements/${name.split('-')[0]}.js`);
+    let stem = name.split('-')[0];
+    let filename = stem.charAt(0).toUpperCase() + stem.slice(1);
+    await import(`${path}/elements/${filename}.js`);
   }
   let ready = new CustomEvent('blamready');
   window.dispatchEvent(ready);

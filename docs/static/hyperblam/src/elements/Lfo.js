6817598b0b32ad551edf0c7cf067c55fbf142ee0
Heydon Pickering <heydon@heydonworks.com>
2026-05-25 13:24:58 +0100

    adding Lfo back again

---
+++ docs/static/hyperblam/src/elements/Lfo.js
@@ -0,0 +1,88 @@
+import { Osc } from '../primitives/Osc.js';
+import { define } from '../tools/define.js';
+
+class Lfo extends Osc {
+  constructor() {
+    super();
+    this.defaultOut = () => [this.parentNode];
+  }
+
+  onblamready() {
+    this.outElems = this.out ? [...document.querySelectorAll(this.out)] : this.defaultOut();
+    for (const outElem of this.outElems) {
+      this.connect(outElem);
+      outElem.addEventListener('blamparams', this);
+    }
+    // ↓ Start all LFOs at the same time
+    this.start(this.c.currentTime);
+  }
+
+  onblamparams(event) {
+    this.connect(event.detail.elem);
+  }
+
+  connect(outElem) {
+    let target = outElem?.params[this.prop];
+    if (target) {
+      this.gainNode.connect(target);
+      if (this.prop === 'beats') {
+        // ↓ Convert to either hertz or seconds
+        let gain;
+        if (outElem.toHertz) {
+          let beats = this.hertzToBeats(target.value);
+          gain = (this.gainNode.gain.value / beats) * target.value;
+        } else {
+          gain = this.conversions.beats(this.gain);
+        }
+        this.gainNode.gain.value = gain;
+      }
+    }
+  }
+
+  get prop() {
+		return this.getAttribute('prop') || 'gain';
+	}
+
+	set prop(value) {
+		this.setAttribute('prop', value);
+  }
+
+  get out() {
+		return this.getAttribute('out');
+	}
+
+	set out(value) {
+		this.setAttribute('out', value);
+  }
+
+  get bypass() {
+		return this.hasAttribute('bypass');
+	}
+
+	set bypass(value) {
+		this.toBoolean('bypass', value);
+	}
+
+  disconnectedCallback() {
+    for (const outElem of this.outElems) {
+      outElem.removeEventListener('blamparams', this);
+    }
+  }
+
+  static get observedAttributes () {
+    return [...super.observedAttributes, 'bypass'];
+  }
+
+  attributeChangedCallback(name, oldVal, newVal) {
+    if (name === 'bypass') {
+      this.params.gain = this.bypass ? 0 : this.gain;
+    }
+    super.attributeChangedCallback(name, oldVal, newVal);
+  }
+
+  static {
+    define(this);
+  }
+}
+
+export { Lfo }

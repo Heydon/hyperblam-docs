92aeef9ad9058fa18ffcda88e9d165c81a27745a
Heydon Pickering <heydon@heydonworks.com>
2026-06-24 08:24:17 +0100

    proper example ordering

--- docs/static/hyperblam/src/elements/Media.js
+++ docs/static/hyperblam/src/elements/Media.js
@@ -11,10 +11,9 @@ class Media extends Base {
 
     if (this.mediaElem.readyState > 3) {
       this.onSource();
+    } else {
+      this.mediaElem.addEventListener('canplaythrough', this.onSource.bind(this));
     }
-
-    this.mediaElem.addEventListener('canplaythrough', this.onSource.bind(this), { once: true });
-    this.mediaElem.addEventListener('canplay', this.onSource.bind(this), { once: true });
 
     this.mediaElem.addEventListener('play', () => {
       this.context().resume();

b8fb6de7f9cfcca894ff1856407103ec114219bc
Heydon Pickering <heydon@heydonworks.com>
2026-09-06 11:12:06 +0100

    more slides work

--- docs/static/hyperblam/src/elements/Log.js
+++ docs/static/hyperblam/src/elements/Log.js
@@ -1,13 +1,17 @@
 import { Template } from '../primitives/Template.js';
 
 class Log extends Template {
+  constructor() {
+    super();
+    this.defaultMode = ['events'];
+  }
+
   handle(event) {
     let data = event?.detail || {};
     data.name = event.type; 
     data.target = event.target;
     data.id = event.target.id ? `#${event.target.id}` : event.target.nodeName.toLowerCase();
     this.interpolate(this.template, data);
-    this.defaultMode = ['events'];
   }
 
   handleWatch(list, observer) {
